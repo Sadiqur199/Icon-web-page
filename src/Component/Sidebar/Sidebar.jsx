aaeec7fdf5ab2495309d541938440f68cf3d8f3a
Sadiqur199 <sadiqurrhaman199@gmail.com>
2023-09-12 20:44:17 +0600

    adeed style , feature and category

--- src/Component/Sidebar/Sidebar.jsx
+++ src/Component/Sidebar/Sidebar.jsx
@@ -1,52 +1,45 @@
-import React from "react";
+import React from 'react';
+import icons from '../../Utill/data';
+import { FaCircle } from 'react-icons/fa';
 
-const Sidebar = ({
-  categoryOptions,
-  selectedCategory,
-  subcategories,
-  selectedSubcategory,
-  onCategoryChange,
-  onSubcategoryChange,
-}) => {
+const style = [...new Set(icons.map(icon=>icon.style))]
+    const featured = [...new Set(icons.map(icon=>icon.featured))]
+const categories = [...new Set(icons.map(icon=>icon.subCategory))]
+
+
+const Sidebar = () => {
   return (
-    <div className="w-full md:w-1/4 p-4">
-      <h2 className="text-lg font-semibold mb-2">Categories</h2>
-      <ul>
-        {categoryOptions.map((category) => (
-          <li key={category}>
-            <button
-              onClick={() => onCategoryChange(category)}
-              className={`block text-blue-500 ${
-                selectedCategory === category ? "font-bold" : ""
-              }`}
-            >
-              {category.charAt(0).toUpperCase() + category.slice(1)}
-            </button>
-          </li>
-        ))}
-      </ul>
-
-      {selectedCategory === "all" &&
-        subcategories &&
-        subcategories.length > 0 && (
-          <>
-            <h2 className="text-lg font-semibold my-2 ">Subcategories</h2>
-            <ul>
-              {subcategories.map((subcategory) => (
-                <li key={subcategory}>
-                  <button
-                    onClick={() => onSubcategoryChange(subcategory)}
-                    className={`block text-blue-500 mr-3 ${
-                      selectedSubcategory === subcategory ? "font-bold" : ""
-                    }`}
-                  >
-                    {subcategory}
-                  </button>
-                </li>
-              ))}
-            </ul>
-          </>
-        )}
+    <div className='pr-24'>
+      <div className='mb-5'>
+       <h1 className='mb-3 font-bold text-gray-500'>STYLE</h1>
+       <ul>
+        {
+          style.map((s,i)=>(
+            <li className='mb-3 text-gray-500 hover:border rounded px-2 border-gray-400' key={i}><FaCircle size={4} className='inline'></FaCircle> {s}</li>
+          ))
+        }
+       </ul>
+      </div>
+      <div className='mb-5'>
+       <h1 className='mb-3 font-bold text-gray-500'>FEATURED</h1>
+       <ul>
+        {
+          featured.map((s,i)=>(
+            <li className='mb-3 text-gray-500 hover:border rounded px-2 border-gray-400' key={i}><FaCircle size={4} className='inline'></FaCircle> {s}</li>
+          ))
+        }
+       </ul>
+      </div>
+      <div className='mb-5'>
+        <h1 className='mb-3 font-bold text-gray-500'>CATEGORIES</h1>
+        <ul>
+        {
+          categories.map((s,i)=>(
+            <li className='mb-3 text-gray-500 hover:border rounded px-2 border-gray-400' key={i}><FaCircle size={4} className='inline'></FaCircle> {s}</li>
+          ))
+        }
+       </ul>
+      </div>
     </div>
   );
 };
